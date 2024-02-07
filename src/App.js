8ee8a0ef05c63848e13c93bc9a4274c48aa48744
김영진 <youngjin1293@naver.com>
2024-02-07 14:37:58 +0900

    영화 map filter sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReviewList from "./components/ReviewList";
+import mockItems from "./mock.json";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((it) => it.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
---
+++ src/components/ReviewList.js
@@ -0,0 +1,38 @@
+import "./ReviewList.css";
+
+const formatDate = (value) => {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+};
+
+const ReviewListItem = ({ item, onDelete }) => {
+  const handleDeleteClick = () => onDelete(item.id);
+  return (
+    <div className="ReviewListItem">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{formatDate(item.createdAt)}</p>
+        <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
+      </div>
+    </div>
+  );
+};
+
+const ReviewList = ({ items, onDelete }) => {
+  console.log(items);
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          <ReviewListItem item={item} onDelete={onDelete} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ReviewList;
